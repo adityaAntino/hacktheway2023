f6d65e05f71d7d131734d69a29d23c0d6e07b65c
Aditya Arya <aarya@coligomed.com>
2023-09-16 17:24:27 +0530

    - Login Completed

--- lib/features/authentication/screen/splash_screen.dart
+++ lib/features/authentication/screen/splash_screen.dart
@@ -1,8 +1,12 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hacktheway2023/config/get_it.dart';
+import 'package:hacktheway2023/config/size_config.dart';
+import 'package:hacktheway2023/constant/app_colors.dart';
 import 'package:hacktheway2023/constant/app_text_style.dart';
+import 'package:hacktheway2023/constant/image_path.dart';
 import 'package:hacktheway2023/constant/string_constant.dart';
 import 'package:hacktheway2023/router/named_route.dart';
 import 'package:hacktheway2023/router/navigation_handler.dart';
@@ -20,15 +24,16 @@ class _SplashScreenState extends State<SplashScreen> {
 
   @override
   void initState() {
-    if((prefs.getString(StringConstant.accessToken) == null) || (prefs.getString(StringConstant.accessToken) == "")) {
-      Timer(
-        const Duration(seconds: 3),
-            () => BulandDarwaza.pushReplacementNamed(context,
-            routeName: RouteName.sendOtpScreen));
-    }else{
+    if ((prefs.getString(StringConstant.accessToken) == null) ||
+        (prefs.getString(StringConstant.accessToken) == "")) {
       Timer(
           const Duration(seconds: 3),
-              () => BulandDarwaza.pushReplacementNamed(context,
+          () => BulandDarwaza.pushReplacementNamed(context,
+              routeName: RouteName.sendOtpScreen));
+    } else {
+      Timer(
+          const Duration(seconds: 3),
+          () => BulandDarwaza.pushReplacementNamed(context,
               routeName: RouteName.dashboardScreen));
     }
     // TODO: implement initState
@@ -38,20 +43,22 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox(
+      body: Container(
+        color: AppColors.kPureBlack,
         width: MediaQuery.of(context).size.width,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text(
-              'Hack The Way',
-              style: AppTextStyle.f16W500Black0E,
-            ),
-            Text(
-              'Mahi Way',
-              style: AppTextStyle.f16W500Black0E,
-            ),
+            SvgPicture.asset(ImagePath.appLogoSvg),
+            SizedBox(height: 8 * SizeConfig.heightMultiplier!),
+            const Text(
+              'Auction Buddy',
+              style: TextStyle(
+                  color: AppColors.kPureWhite,
+                  fontSize: 30,
+                  fontWeight: FontWeight.w600),
+            )
           ],
         ),
       ),
