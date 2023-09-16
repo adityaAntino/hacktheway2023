d00fdb1cdd01c6639629b91676389621fd87c3d8
manasAntino <manas.a@antino.io>
2023-09-16 19:00:57 +0530

    added boiler plate for get all products api

---
+++ lib/features/buy_products/cubit/buy_producuts_state.dart
@@ -0,0 +1,9 @@
+abstract class BuyProductsState {}
+
+class BuyProductsInitial extends BuyProductsState {}
+
+class BuyPrioductsLoading extends BuyProductsState {}
+
+class BuyProductsEmpty extends BuyProductsState {}
+
+class BuyProductsFailed extends BuyProductsState {}
